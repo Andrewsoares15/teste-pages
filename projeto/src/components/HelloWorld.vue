401f8ad430411c7f64bb3046510a6ceb04e3c433
Andrew soares <andrewsoares347@gmail.com>
2021-09-30 15:40:34 -0300

    versao 1.1

--- projeto/src/components/HelloWorld.vue
+++ projeto/src/components/HelloWorld.vue
@@ -1,93 +1,110 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
+    <div class="header">
+      <h1 class="text-left">Challenge</h1>
+      <v-card
+    
+      flat
+    >
+      <v-toolbar
+        flat
+        height="72"
+        class="text-right"
       >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+        <v-switch
+          v-model="$vuetify.theme.dark"
+          inset
+          persistent-hint
+        ></v-switch>
+      </v-toolbar>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
+    </v-card>
+  </div>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="nam"
+      :counter="30"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+      
+    >
+      Validate
+    </v-btn>
+  </v-form>
+   
+    <div v-if="dados">
+      <div class="header-el">
+        <v-avatar size="156" class="mt-4">
+      <img
+        :src="contas.avatar_url"
+        :alt="contas.login"
       >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+    </v-avatar>
+    <ul class="mt-2">
+      <li>@{{contas.login}}</li>
+      <li>{{contas.bio}}</li>
+    </ul>
+    
+      </div>
+     <v-simple-table class="tabela mt-4">
+    <template >
+      <thead>
+        <tr>
+          <th class="text-left">
+            Repositorios
+          </th>
+          <th class="text-left">
+            Seguidores
+          </th>
+          <th class="text-left">
+            Seguindo
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ contas.public_repos }}</td>
+          <td>{{ contas.followers }}</td>
+          <td>{{ contas.following }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+  <ul class="mt-4 info-user">
+    <li>
+      <v-avatar size="20"> 
+      <img src="../assets/local2.png" alt="">
+      </v-avatar><span class="ml-2">{{contas.location}}</span>
+      </li>
+      <li>
+      <v-avatar size="20"> 
+      <img src="../assets/link.png" alt="">
+      </v-avatar><span class="ml-2">{{contas.html_url}}</span>
+      </li>
+      <li>
+      <v-avatar size="20"> 
+      <img src="../assets/origem.png" alt="">
+      </v-avatar><span class="ml-2">{{contas.created_at}}</span>
+      </li>
+      <li>
+      <v-avatar size="20"> 
+      <img src="../assets/twitter.png" alt="">
+      </v-avatar><span class="ml-2">{{contas.twitter_username}}</span>
+      </li>
+  </ul>
+    
+    </div>
   </v-container>
 </template>
 
@@ -96,56 +113,68 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
+       valid: true,
+       dados: false,
+       nam: '',
+       nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
       ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      contas: [],
+      
     }),
+    props: {
+      attrs: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+      methods: {
+      validate () {
+        this.$refs.form.validate()
+        console.log(this.nam)
+        console.log(this.dados)
+        this.chamarApi()
+      }, 
+      chamarApi(){
+         fetch('https://api.github.com/users/' + this.nam)
+        .then(resultado => resultado.json())
+        .then(json => {
+          this.contas = json
+          console.log(this.contas)
+          this.mostraDados()
+        }) 
+      },
+      mostraDados(){
+        this.dados = true;
+      }
+    },
   }
+  
 </script>
+<style scoped>
+.header{
+  display: flex;
+  justify-content: space-between;
+}
+.header-el{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+ul{
+  list-style: none;
+}
+.infos{
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  margin: 10px 0 4px 0;
+  }
+.tabela{
+  background-color: red;
+}
+.info-user{
+  padding: 5px;
+}
+</style>
